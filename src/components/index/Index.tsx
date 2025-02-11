0cafbffbc84b742f06c916fb16d0d530d5b37e0f
Steve De Jongh <de.jongh.steve@gmail.com>
2025-02-11 13:25:15 -0800

    Clean up

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Home } from "../home/home";
 import { Tools } from "../tools/tools";
 
--- src/components/text-sanitizer/TextSanitizerTypes.ts
+++ src/components/text-sanitizer/TextSanitizerTypes.ts
@@ -25,28 +25,3 @@ const ZTextSanitizerForm = z.object({
 type TTextSanitizerForm = z.infer<typeof ZTextSanitizerForm>
 
 export { ZTextSanitizerForm, type TTextSanitizerForm }
-
-
-// import { ZUserPreferences } from "./userPreferences";
-
-// const ZRoleTypes = z.union([z.literal("user"), z.literal("manager"), z.literal("admin")]);
-
-// const ZUserForm = z.object({
-//   email: z.string().email(),
-//   name: z.string(),
-//   roles: ZRoleTypes.array(),
-//   current_password: z.string().optional(),
-//   password: z.string().optional(),
-//   password_confirmation: z.string().optional(),
-// })
-// .superRefine(({ password, password_confirmation, current_password }, ctx) => {
-//   // Edit user validations.
-//   if (!password && !password_confirmation && current_password === "") {
-//     if (current_password === "") {
-//       return ctx.addIssue({
-//         code: z.ZodIssueCode.custom,
-//         path: ["current_password"],
-//         message: 'Current password is required to update account.',
-//       });
-//     }
-//   }
